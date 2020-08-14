e0cdef108c9aa9f86cf2c83800ce595b4a6c745a
Colin Whitcomb <colinsw@berkeley.edu>
2020-08-13 17:30:28 -0700

    [edit] table setup.

--- employee/src/App.js
+++ employee/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './style.css';
 import Jumbotron from "./components/Jumbotron/jumbotron.js";
+import TableHeader from "./components/TableHead/tableHead";
+// import TableWrapper from "./components/TableWrapper/tableWrapper";
 
 function App() {
   return (
     <div>
       <Jumbotron />
+      <TableHeader
+       />
     </div>
   );
 }
---
+++ employee/src/components/TableWrapper/tableWrapper.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function tableWrapper (props) {
+    return (
+        
+        <div class="table">{props.children}</div>
+       
+    )
+}
+
+export default tableWrapper;
