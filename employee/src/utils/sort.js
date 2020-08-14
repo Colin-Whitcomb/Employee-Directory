9230e35a855874f0efcfb70edb3978a705abb5f0
Colin Whitcomb <colinsw@berkeley.edu>
2020-08-14 07:02:01 -0700

    [add] utils/sort.js to manage the sorting functions.

---
+++ employee/src/utils/sort.js
@@ -0,0 +1,72 @@
+var employArr = [{
+        "id": 1,
+        "firstName": "Aaron",
+        "lastName": "Smith",
+        "image": "https://randomuser.me/api/portraits/men/25.jpg",
+        "email": "Aaron_Smith3241@gmail.com",
+        "phone": "(218)-316-2655"
+    },
+    {
+        "id": 2,
+        "firstName": "Alyssa",
+        "lastName": "Chiang",
+        "image": "https://randomuser.me/api/portraits/women/70.jpg",
+        "email": "AlyChiang77@yahoo.com",
+        "phone": "(478-488-3724)"
+    },
+    {
+        "id": 3,
+        "firstName": "Jane",
+        "lastName": "Wills",
+        "image": "https://randomuser.me/api/portraits/women/81.jpg",
+        "email": "JaneWillWin@me.com",
+        "phone": "(775)-338-0991"
+    },
+    {
+        "id": 4,
+        "firstName": "Lenny",
+        "lastName": "Stafford",
+        "image": "https://randomuser.me/api/portraits/men/71.jpg",
+        "email": "L_Stafford77@yahoo.com",
+        "phone": "(212)-572-0559"
+    },
+    {
+        "id": 5,
+        "firstName": "Ethan",
+        "lastName": "Chen",
+        "image": "https://randomuser.me/api/portraits/men/67.jpg",
+        "email": "Chen[Ethan]@gmail.com",
+        "phone": "(620) 454-1799"
+    }
+]
+
+
+//Comparer Function    
+function GetSortOrder(prop) {
+    return function (a, b) {
+        if (a[prop] > b[prop]) {
+            return 1;
+        } else if (a[prop] < b[prop]) {
+            return -1;
+        }
+        return 0;
+    }
+}
+
+// sorts the array by first name A-Z 
+function sortFirstName(Arr) {
+    Arr.sort(GetSortOrder("firstName")); //Pass the attribute to be sorted on       
+    for (var item in Arr) {
+        console.log(JSON.stringify(Arr[item].firstName));
+    }
+}
+
+// sorts the array by last name A-Z
+function sortLastName(Arr) {
+    Arr.sort(GetSortOrder("lastName")); //Pass the attribute to be sorted on       
+    for (var item in Arr) {
+        console.log(JSON.stringify(Arr[item].lastName));
+    }
+}
+
+sortLastName(employArr);
