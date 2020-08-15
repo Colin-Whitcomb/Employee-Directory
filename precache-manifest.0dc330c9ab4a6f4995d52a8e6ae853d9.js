d1fc0f18723e4a0d1de1429e2c72f9a9157c0a0e
Colin Whitcomb <colinsw@berkeley.edu>
2020-08-14 21:31:34 -0700

    Updates

---
+++ precache-manifest.0dc330c9ab4a6f4995d52a8e6ae853d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9ddd30dcd221264e435c62753f66529",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "fa6bd26fe12eeacbb15e",
+    "url": "/Employee-Directory/static/css/main.010b18c8.chunk.css"
+  },
+  {
+    "revision": "7cc94be27aef705c4994",
+    "url": "/Employee-Directory/static/js/2.d0802590.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.d0802590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa6bd26fe12eeacbb15e",
+    "url": "/Employee-Directory/static/js/main.8a7b61e3.chunk.js"
+  },
+  {
+    "revision": "1b13436491d4ecd8c222",
+    "url": "/Employee-Directory/static/js/runtime-main.39ec69b2.js"
+  }
+]);
